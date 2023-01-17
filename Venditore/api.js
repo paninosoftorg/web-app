89509ba57bc1c4dfe2c0d93525fc8cae2e603060
gmarck04 <gmarck04@gmail.com>
2023-01-17 19:11:47 +0100

    Inserimento modifica ordini

--- Venditore/api.js
+++
@@ -1,8 +0,0 @@
-function reqListener() {
-    console.log(this.responseText);
-  }
-  
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", reqListener);
-  req.open("GET", "http://paninos.ddns.net/food-api/API/order/getArchiveOrder.php");
-  req.send();
