98be8e170e739995201da4f3b325007ff7c1f148
gmarck04 <gmarck04@gmail.com>
2023-01-18 13:22:03 +0100

    modifica api

--- Venditore/mostra_ordini.js
+++ Venditore/mostra_ordini.js
@@ -1,15 +1,31 @@
 function reqListener() {
   let data = [this.responseText];
+  
   const obj = JSON.parse(data);
 
+  console.log(obj);
   var li = "";
   for (let i = 0; i < obj.length; i++) {
-     li += obj[i].user + "<br />" + obj[i].created + "<br />";
+    
+      li += obj[i].orders[i].user_name_surname + "<br />";
+    
   }
   document.getElementById("myList").innerHTML = li;
 }
 
 const req = new XMLHttpRequest();
 req.addEventListener("load", reqListener);
-req.open("GET", "http://paninos.ddns.net/food-api/API/order/getArchiveOrder.php");
+req.open(
+  "GET",
+  "http://paninos.ddns.net/food-api/API/order/getArchiveBriefOrder.php"
+);
 req.send();
+
+var date = new Date();
+var today = date.toLocaleDateString("it-IT");
+
+const dp = $("#datepicker").format({ 
+  setDate: today,
+  format:'dd mm yyyy',
+  todayHighlight: true
+});
